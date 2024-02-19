1c25a068192f9817f163a0a6484289dd82916c44
riandarafqi <riandarafqi321@gmail.com>
2024-02-19 08:30:00 +0700

    mengerjakan 3c

--- tugas/tugas1/3c.php
+++ tugas/tugas1/3c.php
@@ -1,17 +1,44 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1c</title>
+    <style>
+        .kotak {
+            width: 50px;
+            height: 50px;
+            background-color: darkmagenta;
+            border: 1px solid black;
+            color: white;
+            display: flex;
+        }
 
-$angka = 91;
-echo "Aku adalah <b>$angka</b><br>";
+        .kotak h2 {
+            margin: auto;
+        }
 
-$angka *= 5;
-echo "Jika aku dikali 5, maka aku sekarang menjadi <b>$angka</b><br>";
 
-$angka /= 2;
-echo "Jika aku dibagi 2, maka aku sekarang menjadi <b>$angka</b><br>";
+        .row {
+            display: flex;
+        }
+    </style>
+</head>
+<body>
 
-$angka += 75;
-echo "Jika aku ditambah 75, maka aku sekarang menjadi <b>$angka</b><br>";
+    <div class="row row-1">        
+        <div class="kotak kotak1"><h2>1</h2></div>
+    </div>
+    
+    <div class="row row-2">
+        <div class="kotak kotak2"><h2>2</h2></div>
+        <div class="kotak kotak2"><h2>2</h2></div><br>
+    </div>
 
-$angka -= 20;
-echo "Jika aku dikurang 20, maka aku sekarang menjadi <b>$angka</b><br>";
-?>
+    <div class="row row-3">
+        <div class="kotak kotak3"><h2>3</h2></div>
+        <div class="kotak kotak3"><h2>3</h2></div>
+        <div class="kotak kotak3"><h2>3</h2></div>
+    </div>
+</body>
+</html>
